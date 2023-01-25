96eb62a2836f0ef401b199e7c37022bf66f416bf
Peyman Khalili <iampeymankhalili@gmail.com>
2023-01-25 15:21:29 +0330

    Import all the libraries that will be used in the project

---
+++ Diabetes_SVM.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_curve , auc
+from sklearn.svm import SVC
